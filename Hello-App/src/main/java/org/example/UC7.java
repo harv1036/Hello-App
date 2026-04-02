ee91be391a98cc06cdb3c995624ffa44c3d47aac
harv1036 <harv1032006@gmail.com>
2026-04-02 12:26:35 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

---
+++ Hello-App/src/main/java/org/example/UC7.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class UC7
+{
+      public static void main(String[] args)
+      {
+        String names = "World";
+        if(args.length > 0)
+        {
+          names = String.join(", ", args);
+        }
+        System.out.println("Hello, " + names + "!");
+      }
+}
